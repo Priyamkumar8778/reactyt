8927ecc56812e14c2b21562bffe8692b7aab1af5
PriyamKumar8778 <priyamkumar8778@gmail.com>
2026-06-22 18:35:38 +0530

    learn import expoer in js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// how to make element using javascript----------->
+/*
 var h1 = document.createElement("h1");
 h1.innerHTML = "Hello, World!";
 console.log(h1);
+document.body.appendChild(h1);
+*/
+
+// how import export works in javascript------------------>
+import a from "./app.js";
+console.log(a);
